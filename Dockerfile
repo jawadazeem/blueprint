80efa2059ff95fa96272bf577cc3f2b91c5c44ed
jawadazeem <jawad.azeem@outlook.com>
2026-04-25 13:58:17 -0400

    feat: update Docker configuration and enhance file upload handling in BillingS3Service

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM maven:3.9.9-eclipse-temurin-21 AS build
 WORKDIR /app
 COPY pom.xml .
 COPY src ./src
-RUN mvn package -DskipTests
+RUN mvn package
 
 # ---------- RUN ----------
 FROM eclipse-temurin:21-jre-alpine
--- src/main/java/com/azeem/billing/service/BillingS3Service.java
+++ src/main/java/com/azeem/billing/service/BillingS3Service.java
@@ -11,7 +11,6 @@ import io.awspring.cloud.s3.S3Resource;
 import io.awspring.cloud.s3.S3Template;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
